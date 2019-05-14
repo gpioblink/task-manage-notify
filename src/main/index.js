2ca2ea40bf44cd33fa916845d70ad3533ed0a5d7
Kai Yamada <gpioblink@aol.com>
2019-05-14 12:28:50 +0900

    Make outline

---
+++ src/main/index.js
@@ -0,0 +1,72 @@
+import { app, BrowserWindow, screen } from 'electron' // eslint-disable-line
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
+}
+
+let mainWindow;
+const winURL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:9080'
+  : `file://${__dirname}/index.html`;
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  const display = screen.getPrimaryDisplay().bounds;
+  const xmax = display.width;
+  // const ymax = display.height;
+  mainWindow = new BrowserWindow({
+    transparent: true,
+    alwaysOnTop: true,
+    height: 48,
+    useContentSize: true,
+    width: 478,
+    x: xmax - 478,
+    y: 0,
+    frame: false,
+  });
+  mainWindow.setAlwaysOnTop(true);
+  mainWindow.loadURL(winURL);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/components/NotifyPage.vue
@@ -0,0 +1,74 @@
+<template>
+<div id="shinchoku-box">
+    <div class="task-box">
+      <div id="next-task">タスク管理システムを完成させる</div>
+    </div>
+    <button class="btn-border">Next</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'notify-page',
+};
+</script>
+
+<style>
+html {
+  -webkit-app-region: drag;
+}
+body {
+  margin: 0;
+  overflow:hidden;
+  -webkit-user-select: none;
+}
+#shinchoku-box {
+  position: absolute;
+  padding: 0.5em;
+  width: 100%;
+  height: 100%;
+  background-color: #fff3db;
+  border: solid 0.03em #fcaf16;
+  overflow: hidden;
+  box-sizing:border-box;
+}
+.task-box {
+  float: left;
+  width: 24em;
+  height: 2em;
+  display: flex;
+  align-items: center;
+  
+}
+#next-task {
+  float: left;
+  position: relative;
+  margin: 0;
+  font-size: 1em;
+  margin-left: 0.2em;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical; 
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-app-region: no-drag;
+}
+.btn-border {
+  float: left;
+  margin: 0em;
+  text-align: left;
+  border: 0.1em solid #9ec34b;
+  font-size: 1em;
+  color: #9ec34b;
+  background-color: #eeeeee;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 0.3em 1em;
+  border-radius: 0.4em;
+  transition: .4s;
+}
+.btn-border:hover {
+  background-color: #9ec34b;
+  border-color: #cbe585;
+  color: #FFF;
+}
+</style>
